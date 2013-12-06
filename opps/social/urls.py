0d5e3cf14fac6350949d4bb73be180653ce2580e
Thiago Avelino <thiagoavelinoster@gmail.com>
2013-12-06 02:54:38 -0200

    Register api urls

--- opps/social/urls.py
+++ opps/social/urls.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url, include
-from tastypie.api import Api
+from django.conf.urls import patterns, url
 
-from .api import Liked, Favorited
+from piston.resource import Resource
+
+from opps.api import ApiKeyAuthentication
+
+from .api import LikedHandler, FavoritedHandler
 
 
-_api = Api(api_name=u"{}/social".format("v1"))
-_api.register(Liked())
-_api.register(Favorited())
+auth = ApiKeyAuthentication()
+liked_resource = Resource(handler=LikedHandler)
+favorited_resource = Resource(handler=FavoritedHandler, authentication=auth)
 
 urlpatterns = patterns(
     '',
-    url(r'^api/', include(_api.urls)),
+    url(r'^api/social/liked/$', liked_resource),
+    url(r'^api/social/favorited/$', favorited_resource),
 )
